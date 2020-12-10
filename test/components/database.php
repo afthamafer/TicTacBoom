441de18e2200ad4f23398051775b61a03758868a
afthamafer <37636774+afthamafer@users.noreply.github.com>
2020-12-10 10:40:51 +0100

    Add files via upload

---
+++ test/components/database.php
@@ -0,0 +1,28 @@
+<?php
+
+// Raspberry pi connection
+
+// $servername = "0.0.0.0";
+// $username = "woorden";
+// $password = "buzword";
+// $db = "tictacboom";
+
+
+// local connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "tictacboom";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$db);
+
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+
+$getname = "SELECT naam FROM naam";
+?>
